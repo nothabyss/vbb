4a38f380eadcac9637376f01f26b082728ff614f
nothabyss <1254469123@qq.com>
2024-06-21 22:56:51 +0800

    2024.6.21

--- fapp.py
+++ fapp.py
@@ -60,9 +60,9 @@ def submit_vote():
     return jsonify(response)
 @app.route('/data/load_blockchain', methods=['POST'])
 def load_blockchain():
-    blockchain_name = request.form.get('blockchain_name')  # 获取名为'name'的参数值
+
     blockchain_id = request.form.get('blockchain_id')  # 获取名为'name'的参数值
-    blockchain_name = find_blockchain_filename(PROJECT_PATH, blockchain_name, blockchain_id)
+    blockchain_name = find_blockchain_filename(PROJECT_PATH, blockchain_id)
     chain = []
     i = 1
     while True:
@@ -93,27 +93,28 @@ def load_blockchain():
     response.headers['Content-Type'] = 'application/json'
     return response
 
-def find_id(id, folder_name):
+def find_id(folder_name):
     match = re.search(r'chain_(\d+)', folder_name)
 
     if match:
         number_after_chain = match.group(1)
         return number_after_chain
     else:
-        print("cannot find the blockchain, please check your name")
+        # print("cannot find the blockchain, please check your name")
         return 0
-def find_blockchain_filename(root_dir, target_substring, id):
+def find_blockchain_filename(root_dir,  id):
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for dirname in dirnames:
             # 判断文件夹名称是否包含特定字符串
-            if target_substring in dirname and id == find_id(id, dirname):
+            if  str(id) == find_id(dirname):
                 # 找到含有特定字符串的文件夹，然后输出文件夹名字
                 return dirname
 
 @app.route('/data/load_Genesis', methods=['POST'])
 def load_Genesis():
-    blockchain_name = request.form.get('blockchain_name')  # 获取名为'name'的参数值
-    blockchain_name = find_blockchain_filename(PROJECT_PATH, blockchain_name)
+    # blockchain_name = request.form.get('blockchain_name')  # 获取名为'name'的参数值
+    blockchain_id = request.form.get('blockchain_id')  # 获取名为'name'的参数值
+    blockchain_name = find_blockchain_filename(PROJECT_PATH,  blockchain_id)
     chain = []
     chain_file_path = os.path.join(PROJECT_PATH, f'records/chains/{blockchain_name}/GenesisBlock.dat')
     try:
@@ -141,8 +142,43 @@ def load_Genesis():
     response = make_response(json_block, 200)
     response.headers['Content-Type'] = 'application/json'
     return response
+
+@app.route('/protocol/verify', methods=['POST'])
+def verify():
+    blockchain_id = request.form.get('blockchain_id')  # 获取名为'name'的参数值
+    blockchain_name = find_blockchain_filename(PROJECT_PATH, blockchain_id)
+    chain = []
+    i = 1
+    while True:
+        chain_file_path = os.path.join(PROJECT_PATH, f'records/chains/{blockchain_name}/block-{i}.dat')
+        try:
+            with open(chain_file_path, 'rb') as file:
+                block = pickle.load(file)
+                block_dict = {
+                    'height': block.height,
+                    'votedata': block.votedata,
+                    'votecount': block.votecount,
+                    'number_of_votes': block.number_of_votes,
+                    'merkle': block.merkle,
+                    'difficulty': block.DIFFICULTY,
+                    'timeStamp': block.timeStamp,
+                    'prevHash': block.prevHash,
+                    'hash': block.hash,
+                    'nonce': block.nonce
+                }
+
+                json_block = json.dumps(block_dict)
+                chain.append(json_block)
+                i += 1
+        except FileNotFoundError:
+            break
+    chain = jsonify(chain)
+    response = make_response(chain, 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 if __name__ == '__main__':
-    # a = find_blockchain_filename(PROJECT_PATH, "23aaee7dc8fc47201fb4ec60e53ff3ced373fba0fad0f977c6919e84785f0c", "200")
+    # a = find_blockchain_filename(PROJECT_PATH,  200)
     # print(a)
     # print(PROJECT_PATH)
     # print(sys.path)
