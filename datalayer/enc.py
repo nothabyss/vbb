fb87b85bebdc88084d8d1e74a8bce82e0c38174a
nothabyss <1254469123@qq.com>
2024-07-02 22:24:08 +0800

    2024.7.2

--- datalayer/enc.py
+++ datalayer/enc.py
@@ -18,10 +18,10 @@ def rsakeys():
     decoded_privatekey = privatekey.export_key().decode('utf-8')
 
     # output_string = input_string.replace("\n", "")
-    publickey = str(decoded_publickey.replace('\n', ''))
-    privatekey = str(decoded_privatekey.replace('\n', ''))
-    # publickey = str(decoded_publickey)
-    # privatekey = str(decoded_privatekey)
+    # publickey = str(decoded_publickey.replace('\n', ''))
+    # privatekey = str(decoded_privatekey.replace('\n', ''))
+    publickey = str(decoded_publickey)
+    privatekey = str(decoded_privatekey)
 
     return privatekey, publickey
 
@@ -120,7 +120,7 @@ def pre_impose_sk(sk):
 
 if __name__ == '__main__':
     # ---------
-    sk, pk = rsakeys()
+    # sk, pk = rsakeys()
     # sk = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDanISmp2X1Dwb5CvbXSYtkLkq6MOa/bbPJ9Is7TMRQA+mw0TIGVAieo3V95EezrMcowxpfCaCien7tc4ab3IdIFlx33JPXUvcV1+TpKcS94CDmdkswQHEu5FQX4xRFhYoJOx+IyqKA5uW8fnoVCZF+hcDzUwJsFBe0kFCH7QSZOQIDAQAB'
     # pk = 'MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBANqchKanZfUPBvkK9tdJi2QuSrow5r9ts8n0iztMxFAD6bDRMgZUCJ6jdX3kR7OsxyjDGl8JoKJ6fu1zhpvch0gWXHfck9dS9xXX5OkpxL3gIOZ2SzBAcS7kVBfjFEWFigk7H4jKooDm5bx+ehUJkX6FwPNTAmwUF7SQUIftBJk5AgMBAAECgYEAoRIXjq7iKWgUDCSu8LrIUFznRD5JlZvfjxp2B5AtSINJiLTp9c2uuCvZopMy3UidSQRPhtySFYTJxmyLLoWSygbGq8jYHgflVXI8EUWQdCrvEfrrVkmGtHeUszt+HlarXuL+Raqkgf6R863rg+isYQrsg+D1tq0+urbn3zSjm4ECQQDvOv1hzGkge4gwZLrSKWr3hzrxKp/RoEnRIXITLgCvlUw9NpvzXucSFKcEPJgcHwYj0haVqWjlLKa7zgFd5gvRAkEA6e+EppTmuBu+5e4WJcMhRHBwjM1iwTVvXzTcBo1bKCIEPRKbwLnUSsHVx2+t8ggSwQEZ1qTary8K2j+Y4lRY6QJBANGlCFekIpxspTSDkZSK50p0H5sol1XE+etjO+zC66bzVxRtvszP5f9aSeLUlxhNt7u8aD5Pb7UmJBeAuIHpD4ECQA0gCCREhHFd9Sb3Zby2pv4tBNORjnHiqp3/3Dxt3+hviCdZDO4SPwv2tiTbajoDI/I6OpXlZ/OeQxCdstk3/EkCQQCHP33Cb5A7FLJdt8j5GZzDDC7e5YuMWcg1i4+QQrxtsSrRkTOqHtFsWR8tF2suJS9Ermx1w89DjxqUKU7l4MPe'
     # sk = "MIICXgIBAAKBgQDBjjb9XFms2EShERr+cKyf6Uhp7TPDfUnlX6+F2uOZTcDxNJPx78TQNlWYcImlQyL9iOU+KR1LTERiFXDplyBKlomCeITs1g9ZeD/baWAvMSxK5u1STeli1rK7GvAnXRyCtDP2VtlEn8tnJ2UrnR3BuMLozsy01VPg8FNH/f1OswIDAQABAoGAQEsmvgNSsGUIav49Gr6/wMCjcrg9xRaVmNEvg0Wh67cVu6ms6SUtmcIqrZ/YQSKreU9jYsISfoQfhPs9TgKK0VeQ0n2z8E3/lsTEBCz64dERNvjYocaEdwtyv0naFZeGfcTQeuQK0uJ/7h2jeF1jp6sIbbRAquINdgzHXrsiYRkCQQDNT/X++6RyPDmba4wWjl/GWDI4pmVdeR0FoRSNf5xteXhQUtWHuwXxbg2DQzXyLINt0pn5b0HsaWo8YIWlNVt7AkEA8Vc0qOwar+YMYPNeGXYqFY24haQASa9csilO3bfD5e2p0qVRCmnLihIm9+Ujm7ChM0V8UT3re+gOwdrbk7B4KQJBALC/bJPVSM17IVC2NkRYzwSuepWgUdU1ZZGqCUqDGER6dOQZW2/cxpIAXyuoWbGhNof97MX6TLNcULOnmIYQOC8CQQCc3/qy8wZF4FubiZZZJp4kdILFG920B/FRPdit9o24H8yLyDntcueDUFlUOfXzddTS5w/wHze6yxUMnZ6tWcBBAkEApzoAKhWo+7VSu5lVoYYzBzn0ZuRDk1em6m3ybpoZSUI2GaKqhioKGlO+4i4uB44gSUQSMUI0Iyysfl8qKOTtkw=="
@@ -134,10 +134,10 @@ if __name__ == '__main__':
     # print(repr(pk2))
     # print(pk2 == pk)
     # sk, pk = rsa_original_keys()
-    print(sk)
-    print(pk)
-
-    Signature = 'M3F49JQNgFEBfhXXUoQ3b+CaOmhtok/RqvWTVgGIzsIPWpVFfewe7z4ly4+v2kLSTxYUAuNTThJ5M3KpocluhoiAjmvRp87q7Ulp3Fky6mBoXZWpuYY5JgqzuqWcbMDIBIwymv3WVgajdjwUnLYHJ5JmuVUFShO0nvL3OXVPIpc='
+    # print(sk)
+    # print(pk)
+    pk = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDR2JvM5cAzbKF7tCPMnpVLE7RlDEpj2DnJQSc7Xwrysy9V+N/l0jEkqfGwDBcvlkxiGxOtjpp4xgBTjjPGTJngrgAVFuyw8jEpUUdsxYap93ijv1tSrvhjnhW8dqu5n0MC0XrTciE6qzWfdl/ktQHFQd54W7j2x3usZiZufKZwOwIDAQAB'
+    Signature = 'XPdlT5AUtjD//jTk160s5TCdt9Q9PAVmEbbOaVO5HWL/C3XDDj9mPjEsb14AxwtTBIKUpZe/Tb8/c0tz6Jb2wYMMA5KrhSdTZ2bYjs96LW+ou8OK+VapL2uXQ1x7o3TbDIshf91DirS7OselKSEYYRkCElnvjIyl29DylM2rHk8='
     text = "123"
     print(len("mx6zfk6c++3/dTGNkTJiRTXyj+8/k9kU47Zg2umsySqgVpWAklS1o7l8yQIDAQAB"))
     # en = encrypt(pk, text)
